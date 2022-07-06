b2b131297798861225b6b199030a8372c07dae49
Santo Sidauruk <santosidauruk@gmail.com>
2022-07-06 16:56:38 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/santosidauruk/hello-go-modules-app
+
+go 1.17
+
+require github.com/santosidauruk/hello-go-modules v1.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/santosidauruk/hello-go-modules"
+)
+
+func main() {
+	h := hello.SayHello()
+	fmt.Println(h)
+}
